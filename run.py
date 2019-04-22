e27c68f6245e9d73f5efd9f9f5f3d5c6d716bf83
箐风 <31740706+luocong1996@users.noreply.github.com>
2019-04-22 12:11:39 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,11 @@ class Run(Crf_model):
 
     def __init__(self):
         Crf_model.__init__(self)
-        self.train()
+        if self.pattern == 'train':
+            self.train()
+        else:
+            self.demo()
+
 
 if __name__ == '__main__':
     Run()
